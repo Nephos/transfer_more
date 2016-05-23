2e92c22c41849939145b74d42359aef5b7271957
Arthur Poulet <arthur.poulet@cryptolab.net>
2016-05-23 21:53:15 +0200

    fix plain text
    - add transformation txt -> plain
    - add charset utf-8 by default for plain text
    - fix mime render via extnam

--- src/transfer_more/mime_search.cr
+++ src/transfer_more/mime_search.cr
@@ -10,11 +10,12 @@ module TransferMore
 
     EXT_TRANSFORM = {
       %w(jpg jpe) => "jpeg",
+      %w(txt) => "plain",
     }
 
     EXT = {
       "image" => %w(png jpeg gif tiff),
-      "text" => %(css text html csv),
+      "text" => %(css html csv plain),
       "application" => %w(javascript json pdf xml zip),
       "audio" => %w(mpeg ogg),
       "video" => %w(mpeg mp4 webm),
@@ -25,7 +26,7 @@ module TransferMore
       [0x47, 0x49, 0x46, 0x38, 0x39, 0x61] => "image/gif",
       [0x47, 0x49, 0x46, 0x38, 0x37, 0x61] => "image/gif",
       [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A] => "image/png",
-      [0x23, 0x21] => "text/plain; charset=utf-8",
+      [0x23, 0x21] => "text/plain",
       [0x25, 0x50, 0x44, 0x46] => "application/pdf",
       [0xFF, 0xFB] => "audio/mpeg",
       [0x49, 0x44, 0x33] => "audio/mpeg",
@@ -36,6 +37,7 @@ module TransferMore
 
     def get_content_type(default = "application/bin") : String
       mime = get_mime_from_magic_number || get_mime_from_extname
+      mime = "#{mime}; charset=utf-8" if mime == "text/plain"
       mime || default
     end
 
@@ -44,7 +46,7 @@ module TransferMore
       replaced = EXT_TRANSFORM.select{|k,v| k.includes? extname}.first[1] rescue nil
       ext_usable = replaced || extname
       mime = EXT.select{|k,v| v.includes? ext_usable}.first rescue nil
-      mime && "#{mime[0]}/#{extname}"
+      mime && "#{mime[0]}/#{ext_usable}"
     end
 
     def get_mime_from_magic_number : String?
