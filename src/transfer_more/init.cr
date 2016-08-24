80175d81c368458206f6927c88dba38bf240f4fc
Arthur Poulet <arthur.poulet@mailoo.org>
2016-08-25 00:18:49 +0200

    Improve EXT_TRANSFORM

--- src/transfer_more/init.cr
+++ src/transfer_more/init.cr
@@ -1,4 +1,4 @@
 module TransferMore
-  BASE_URL = ENV["TRANSFER_BASE_URL"] rescue (STDERR.puts "warning: no TRANSFER_BASE_URL set"; "http://localhost:3000")
+  BASE_URL     = ENV["TRANSFER_BASE_URL"] rescue (STDERR.puts "warning: no TRANSFER_BASE_URL set"; "http://localhost:3000")
   BASE_STORAGE = ENV["TRANSFER_BASE_STORAGE"]? || "/tmp"
 end
