8304c1f83cee7937144d9aa80da686bbece64b1f
Arthur Poulet <arthur.poulet@mailoo.org>
2016-10-11 10:56:55 +0100

    Improve global architecture
    - Update README
    - Add `make run`
    - move web actions on web/
    - move lib classes and constants on lib/

--- src/transfer_more/init.cr
+++ src/transfer_more/init.cr
@@ -2,3 +2,5 @@ module TransferMore
   BASE_URL     = ENV["TRANSFER_BASE_URL"] rescue (STDERR.puts "warning: no TRANSFER_BASE_URL set"; "http://localhost:3000")
   BASE_STORAGE = ENV["TRANSFER_BASE_STORAGE"]? || "/tmp"
 end
+
+require "./lib/*"
