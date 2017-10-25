e8829db1540405f1c3e5508ba711871da25c940d
Arthur Poulet <arthur.poulet@sceptique.eu>
2017-10-25 22:25:47 +0200

    Add web browser upload route

--- src/transfer_more/web/upload.cr
+++ src/transfer_more/web/upload.cr
@@ -1,7 +1,7 @@
 require "secure_random"
 
-private def get_upload_infos(env)
-  file_name = env.params.url["file_name"].downcase
+def get_upload_infos(filename : String)
+  file_name = filename.downcase
   dir = Time.now.to_s(TransferMore::TIME_FORMAT) + "/" + SecureRandom.hex(TransferMore::SECURE_SIZE)
   Dir.mkdir_p(TransferMore.storage "files/#{dir}")
   visible_path = "#{dir}/#{file_name}"
@@ -13,17 +13,25 @@ end
 
 # Fast
 put "/:file_name" do |env|
-  file_name, dir, visible_path, file_path = get_upload_infos env
+  file_name, dir, visible_path, file_path = get_upload_infos env.params.url["file_name"]
   File.open(file_path, "w") { |f| IO.copy(env.request.body.as(IO), f) }
   TransferMore::BASE_URL + "/" + visible_path + "\n"
 end
 
 # Slow
 post "/:file_name" do |env|
-  file_name, dir, visible_path, file_path = get_upload_infos env
-  file = env.params.files.first.last.tmpfile
-  File.open(file_path, "w") { |f| IO.copy(file, f) }
+  _, http_file_infos = env.params.files.first
+  file_name, dir, visible_path, file_path = get_upload_infos env.params.url["file_name"]
+  File.open(file_path, "w") { |f| IO.copy(http_file_infos.tmpfile, f) }
   TransferMore::BASE_URL + "/" + visible_path + "\n"
+end
+
+# Web upload
+post "/" do |env|
+  _, http_file_infos = env.params.files.first
+  file_name, dir, visible_path, file_path = get_upload_infos http_file_infos.filename.to_s
+  File.open(file_path, "w") { |f| IO.copy(http_file_infos.tmpfile, f) }
+  env.redirect TransferMore::BASE_URL + "/" + visible_path
 end
 
 get "/:part1/:part2/:file_name" do |env|
