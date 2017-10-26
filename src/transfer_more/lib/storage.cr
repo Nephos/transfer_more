cc58c840c7ec1746f337dcc037f81f17e4cddcb9
Arthur Poulet <arthur.poulet@sceptique.eu>
2017-10-27 00:48:29 +0200

    Add auto-detect host and port of the server (by request)

--- src/transfer_more/lib/storage.cr
+++ src/transfer_more/lib/storage.cr
@@ -2,4 +2,8 @@ module TransferMore
   def self.storage(f)
     File.expand_path f, BASE_STORAGE
   end
+
+  def self.base_url(env)
+    "http#{Kemal.config.ssl ? 's' : nil}://#{(env.request.host_with_port || "localhost:3000")}"
+  end
 end
