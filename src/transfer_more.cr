f9defce2237c666032277d404b3e2e7f37931d36
Arthur Poulet <arthur.poulet@sceptique.eu>
2018-11-11 23:45:18 +0100

    Fix some upload with browsers

--- src/transfer_more.cr
+++ src/transfer_more.cr
@@ -1,6 +1,14 @@
 require "kemal"
 require "./transfer_more/*"
 
+static_headers do |response, filepath, filestat|
+  if filepath =~ /\.html$/
+    response.headers.add("Access-Control-Allow-Origin", "*")
+  end
+  response.headers.add("Content-Size", filestat.size.to_s)
+end
+serve_static true
+
 # TODO: proper directory check
 Dir.mkdir(TransferMore.storage("files")) rescue nil
 Kemal.run
