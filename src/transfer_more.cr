069309c6afd95a09911e34ed3cee84769a1ae712
Arthur Poulet <arthur.poulet@sceptique.eu>
2022-07-09 10:18:26 +0200

    Fix static file serving, add favicon

--- src/transfer_more.cr
+++ src/transfer_more.cr
@@ -1,13 +1,11 @@
 require "kemal"
 require "./transfer_more/*"
 
-static_headers do |response, filepath, filestat|
-  if filepath =~ /\.html$/
-    response.headers.add("Access-Control-Allow-Origin", "*")
-  end
-  response.headers.add("Content-Size", filestat.size.to_s)
-end
-serve_static true
+# static_headers do |response, filepath, filestat|
+#   response.headers.add("Access-Control-Allow-Origin", "*")
+#   response.headers.add("Content-Size", filestat.size.to_s)
+# end
+# serve_static true
 
 # TODO: proper directory check
 Dir.mkdir(TransferMore.storage("files")) rescue nil
